041475d07d7bd943c75c1d7af517550c30a18855
--ifigazsi <ifigazsisp@gmail.com>
2022-06-12 20:04:13 +0200

    kesz mindegyik

--- megoldas.js
+++ megoldas.js
@@ -1,17 +1,14 @@
-//1. Feladat
 function DiakInfo(nev, csoport, tipus) {
     var szam = String(csoport).length == 1 ? szam = "0".concat(csoport) : szam = String(csoport);
     var szoveg = "".concat(nev, " [TEAM").concat(szam, "] - ");
     tipus ? szoveg += "Junior Frontend" : szoveg += "Webprogramozó";
     return szoveg;
 }
-//2. Feladat
 function SzovegesErtekeles(jegy) {
     var szorgalom = ["NA", "NA", "Hanyag", "Változó", "Jó", "Példás"];
     var magatartas = ["NA", "NA", "Rossz", "Változó", "Jó", "Példás"];
     return [szorgalom[jegy], magatartas[jegy]];
 }
-//3. Feladat
 function HarommalOszthatokSzama(t) {
     var db = 0;
     for (var _i = 0, t_1 = t; _i < t_1.length; _i++) {
@@ -20,9 +17,8 @@ function HarommalOszthatokSzama(t) {
     }
     return db;
 }
-//4. Feladat
 function Nyeroszamok(mennyiseg, alsoHatar, felsoHatar) {
-    var szamok = [];
+    var szamok = []; //feladatban nincs leírva, hogy szám tipust tartalmazó lista kell, hogy legyen 
     while (szamok.length != mennyiseg) {
         var generaltSzam = String(Math.floor(Math.random() * (felsoHatar + 1 - alsoHatar) + alsoHatar));
         if (!szamok.includes(generaltSzam)) {
@@ -31,4 +27,3 @@ function Nyeroszamok(mennyiseg, alsoHatar, felsoHatar) {
     }
     return szamok;
 }
-document.write("fvd: " + Nyeroszamok(5, 1, 10));
